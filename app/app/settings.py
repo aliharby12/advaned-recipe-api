6d6f57bb02fdedfa5fc47821fb0612dc86036eb6
aliharby12 <aliharby565@gmail.com>
2020-07-05 19:57:00 +0200

    create user app and its create api

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+
+    # new apps
+    'core',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'user'
 ]
 
 MIDDLEWARE = [
