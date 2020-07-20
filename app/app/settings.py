e4a50cd0076d3808790685380222ffd621aa2827
aliharby12 <aliharby565@gmail.com>
2020-07-20 08:30:24 +0200

    create tag endpoint with it's views and serializer

--- app/app/settings.py
+++ app/app/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'core',
     'rest_framework',
     'rest_framework.authtoken',
-    'user'
+    'user',
+    'recipe'
 ]
 
 MIDDLEWARE = [
--- app/app/urls.py
+++ app/app/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/api/', include('user.urls'))
+    path('users/api/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ]
